cdaddb1058ec6d1ccdd084d7b1f763ae07eed238
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-08-31 10:07:31 +0530

    update privacy-policy, terms

--- components/landing.js
+++ components/landing.js
@@ -7,18 +7,20 @@ export default function LandingPage() {
   return (
     <ThemeProvider theme={theme}>
       <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-        bgcolor="background.default"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+          bgcolor: "background.default",
+        }}
       >
         <Typography variant="h2" color="text.primary" gutterBottom>
           Welcome to YouTube Watch Party
         </Typography>
         <Typography variant="h5" color="text.secondary" gutterBottom>
-          Create and join watch parties with your favorite YouTubers!
+          Create watch parties for your audience from your YouTube library!
         </Typography>
         <Button variant="outlined" color="primary">
           Get Started

cb086f8cd19c652738f988810958131eef1e9662
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-08-31 10:09:08 +0530

    rm theme from landing page

--- components/landing.js
+++ components/landing.js
@@ -1,31 +1,28 @@
 import React from "react";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "../utils/theme";
+
 import { Box, Typography, Button } from "@mui/material";
 
 export default function LandingPage() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "100vh",
-          bgcolor: "background.default",
-        }}
-      >
-        <Typography variant="h2" color="text.primary" gutterBottom>
-          Welcome to YouTube Watch Party
-        </Typography>
-        <Typography variant="h5" color="text.secondary" gutterBottom>
-          Create watch parties for your audience from your YouTube library!
-        </Typography>
-        <Button variant="outlined" color="primary">
-          Get Started
-        </Button>
-      </Box>
-    </ThemeProvider>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        bgcolor: "background.default",
+      }}
+    >
+      <Typography variant="h2" color="text.primary" gutterBottom>
+        Welcome to YouTube Watch Party
+      </Typography>
+      <Typography variant="h5" color="text.secondary" gutterBottom>
+        Create watch parties for your audience from your YouTube library!
+      </Typography>
+      <Button variant="outlined" color="primary">
+        Get Started
+      </Button>
+    </Box>
   );
 }
